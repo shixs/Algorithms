014b770b51d54da5344e26c0f7189e14a1924860
shixs <bsxdid@gmail.com>
2015-12-09 07:23:23 -0500

    add the solution by using one pointer.

--- RemoveElement.java
+++ RemoveElement.java
@@ -2,7 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class RemoveElement{
+	//using one pointer
+	public static int removeElement3(int[] nums, int val){
+		if(nums == null || nums.length < 1){
+			throw new IllegalArgumentException();
+		}
+
+		int p = 0;
+		for(int i = 0;i < nums.length;i++){
+			if(nums[i] != val){
+				nums[p] = nums[i];
+				p++;
+			}
+		}
+		return p;
+	}
 	public static int removeElement(int[] nums, int val){
+		if(nums == null || nums.length < 1){
+			throw new IllegalArgumentException();
+		}
+		int newLen = 0;
+        	int i = 0;
+        	int j = nums.length - 1;
+
+        	while(i <= j){
+			if(nums[i] == val){
+                		if(nums[j] != val){
+                    			int n = nums[i];
+                    			nums[i] = nums[j];
+              				nums[j] = n;
+                    			i++;
+                    			j--;
+                    			newLen++;
+                		}else{
+                    			j--;
+                		}
+            		}else{
+                		newLen++;
+                		i++;
+            		}
+        	}
+
+        	return newLen;
+	}
+	
+	public static int removeElement2(int[] nums, int val){
 		if(nums == null || nums.length < 1){
 			throw new IllegalArgumentException();
 		}
@@ -42,7 +86,7 @@ public class RemoveElement{
 	public static void main(String[] args){
 		int[] nums = {1,3,2,4,1,1,6};
 		try{
-			System.out.println(removeElement(nums,1));
+			System.out.println(removeElement3(nums,1));
 		}catch(IllegalArgumentException e){
 			System.out.println(e.toString());
 		}
