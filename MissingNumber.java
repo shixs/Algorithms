0e5f0b257e9ca7b597f2260ca36c3f72deeb867e
shixs <bsxdid@gmail.com>
2016-02-09 13:54:52 -0500

    Implemented another two ways to solve Missing Number problem. One is using sum of those integers, the other one is using binary search.

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MissingNumber{
   public static int missingNumber(int[] nums){
     if(nums == null || nums.length < 1){
@@ -17,8 +18,38 @@ public class MissingNumber{
     return nums[nums.length - 1] + 1;
   }
 
+  public static int findMissingNumberBySum(int[] nums){
+    if(nums == null || nums.length < 1){
+      return 0;
+    }
+    int sum = (1 + nums.length) * nums.length / 2;
+    for(int i = 0;i < nums.length;i++){
+      sum -= nums[i];
+    }
+    return sum;
+  }
+  
+  public static int findMissingNumberByBS(int[] nums){
+    if(nums == null || nums.length < 1){
+      return 0;
+    }
+    Arrays.sort(nums);
+    int start = 0, end = nums.length;
+    while(start < end){
+      int middle = (start + end) / 2;
+      if(nums[middle] > middle){
+        end = middle;
+      }else{
+        start = middle + 1;
+      }
+    }
+    return start;
+  }
+
   public static void main(String[] args){
     int[] nums = {0,1,3};
-    System.out.println(missingNumber(nums));
+    System.out.println(findMissingNumberByBS(nums));
+    //System.out.println(findMissingNumberBySum(nums));
+    //System.out.println(missingNumber(nums));
   }
 }
