234882a51725d6fde8de3fd027be3146a73385e6
shixs <bsxdid@gmail.com>
2016-02-10 11:54:00 -0500

    Solution of Single Number.

---
+++ SingleNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SingleNumber{
+  public static int singleNumber(int[] nums){
+    if(nums == null){
+      return 0;
+    }
+    HashSet<Integer> set = new HashSet<>();
+    int sum = 0;
+    for(int i = 0;i < nums.length;i++){
+      if(!set.add(nums[i])){
+        sum -= nums[i];
+      }else{
+        sum += nums[i];
+      }
+    }
+    return sum;
+  }
+
+  public static void main(String[] args){
+    int[] nums = {1,1,2,3,4,3,4,6,2};
+    System.out.println(singleNumber(nums));
+  }
+}
