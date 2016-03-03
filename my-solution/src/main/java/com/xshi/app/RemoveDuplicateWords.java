23ffd35a15ea72beddee98684742c811dfcf59d0
shixs <bsxdid@gmail.com>
2016-03-03 14:40:39 -0800

    remove the duplicate words from a sentence. run a junit test.

--- my-solution/src/main/java/com/xshi/app/RemoveDuplicateWords.java
+++ my-solution/src/main/java/com/xshi/app/RemoveDuplicateWords.java
@@ -1,11 +1,15 @@
+package com.xshi.app;
+import java.util.*;
 public class RemoveDuplicateWords{
   public String removeDuplicateWords(String sentence){
     StringBuffer stringBuffer = new StringBuffer();
     HashSet<String> set = new HashSet<>();
     String[] words = sentence.split(" ");
+    set.add(words[0]);
+    stringBuffer.append(words[0]);
     for(String word : words){
       if(set.add(word)){
-        stringBuffer.append(word);
+        stringBuffer.append(" ").append(word);
       }
     }
     return stringBuffer.toString();
--- my-solution/src/test/java/com/xshi/app/AppTest.java
+++ my-solution/src/test/java/com/xshi/app/AppTest.java
@@ -1,41 +1,16 @@
-package com.xshi.app;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.xshi.app.RemoveDuplicateWords;
+import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+public class AppTest {
     @Test
     public void testApp()
     {
         RemoveDuplicateWords rdw = new RemoveDuplicateWords();
         String sentence = "this is a book a this";
-        assertEquals("this is a book",rdw.removeDuplicateWords(sentence));
+        Assert.assertEquals("this is a book",rdw.removeDuplicateWords(sentence));
     }
 }
