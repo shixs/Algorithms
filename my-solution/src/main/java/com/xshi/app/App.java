768e52ee1634206b9de4aaefbb4ac74ee47314aa
shixs <bsxdid@gmail.com>
2016-03-18 10:56:52 -0700

    Solution for Best Time to Buy and Sell Stock II.

--- my-solution/src/main/java/com/xshi/app/App.java
+++ my-solution/src/main/java/com/xshi/app/App.java
@@ -3,11 +3,14 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 public class App {
-    public static void main( String[] args ){
-        Result result = JUnitCore.runClasses(DivideTwoIntegers.class);
+  public static void main( String[] args ){
+        /*Result result = JUnitCore.runClasses(DivideTwoIntegers.class);
         for(Failure failure : result.getFailures()){
           System.out.println(failure.toString());
         }
-        System.out.println(result.wasSuccessful());
-    }
+        System.out.println(result.wasSuccessful());*/
+    int[] prices = {6,11,12,8,7,15};
+    BestTimeToBuyAndSellStockII best = new BestTimeToBuyAndSellStockII();
+    System.out.println(best.maxProfit(prices));      
+  }
 }
